b89bd7fee09ea43e9a433821bd028886ab0ca99e
KodyFFy <62069785+KodyFFy@users.noreply.github.com>
2020-12-24 21:31:52 +0300

    Update bin.py

--- bin.py
+++ bin.py
@@ -1,5 +1,5 @@
 info = {
-	'TOKEN':'Nzg5ODI3NzA2NTUyMzIwMDIw.X93uXw.K2b7cdBdsrydLXgOF4rfieQdzgQ',
+	'TOKEN':'Nzg5ODI3NzA2NTUyMzIwMDIw.X93uXw.M7pp3UnfROzqE0CJDjXJoLbKZsA',
 	'PREFIX':'e!',
 	'ID':'789827706552320020'
 }
